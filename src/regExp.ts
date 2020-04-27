597be1eeebf4fb2192c29a4edae091e5303533f6
weiweiqin <weiweiqin@vv.cn>
2020-04-27 10:14:35 +0800

    修复长中文问题

--- src/regExp.ts
+++ src/regExp.ts
@@ -10,7 +10,7 @@ export const chinaSymbolsReg = /[丨\u3002\uff1f\uff01\uff0c\u3001\uff1b\uff1a\u
 export const matchCusChinaReg = /([\u4e00-\u9fa5]+[.]{1}[\u4e00-\u9fa5.]*[\u4e00-\u9fa5]+)/g;
 
 // 增加自定义规则匹配，比如（很长的中文文字需要/截取）
-export const matchCusLongChinaReg = /([\u4e00-\u9fa5]+[/]{1}[\u4e00-\u9fa5]+)/g;
+export const matchCusLongChinaReg = /([\u4e00-\u9fa5\u3002\uff1f\uff01\uff0c\u3001\uff1b\uff1a\u201c\u201d\u2018\u2019\uff08\uff09\u300a\u300b\u3008\u3009\u3010\u3011\u300e\u300f\u300c\u300d\ufe43\ufe44\u3014\u3015\u2026\u2014\uff5e\ufe4f\uffe5]+[/]{1}[\u4e00-\u9fa5\u3002\uff1f\uff01\uff0c\u3001\uff1b\uff1a\u201c\u201d\u2018\u2019\uff08\uff09\u300a\u300b\u3008\u3009\u3010\u3011\u300e\u300f\u300c\u300d\ufe43\ufe44\u3014\u3015\u2026\u2014\uff5e\ufe4f\uffe5]+)/g;
 
 // 国际化标识正则
 export const formatMessageReg = /formatMessage\(\{[\s]*id:\s*['|"]([1-9a-zA-Z.%]*)['|"][\s]*\}\)/g;
